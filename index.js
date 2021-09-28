f6ace8ee7229c133a385b6eeee1c4814c1140eef
Francois Janson <jansonfrancois@gmail.com>
2021-09-28 14:12:06 +0200

    handling login

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const port = 8000;
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.get("/", (req, res) => {
+    res.render("home")
+});
+app.get("/login", (req, res) => {
     res.render("login")
 });
 
+app.post("/login", (req, res) => {
+    console.log(`username : ${req.body.username}, password : ${req.body.password}`)
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
